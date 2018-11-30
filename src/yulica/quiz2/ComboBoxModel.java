093eb538832fdf88534c7ac264f1680b6719e377
yulicaa <yulidcahya99@gmail.com>
2018-11-30 20:00:18 +0700

    menambahkan array list untuk mendapatkan jenis nama

--- src/yulica/quiz2/ComboBoxModel.java
+++ src/yulica/quiz2/ComboBoxModel.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
  *
  * @author Yulica
  */
+// Deklarasi class Combo box model untuk merepresentasikan item
 class ComboBoxModel {
     private HashMap<String, Float> items = new HashMap<>(); //variabel HashMap untuk menyimpan objek item
 
@@ -17,7 +18,16 @@ class ComboBoxModel {
         this.items.put("Gula", new Float(20000));
     }
     
-
+    // ArrayList untuk mendapatkan jenis nama barang
+    public ArrayList<String> getJenisNama(){
+        ArrayList<String> str = new ArrayList<>();
+        for(String item : this.items.keySet()){
+            str.add(item);
+        }
+        return str;
+    }
+    
+    
     
     // menambahkan item ke daftar belanja
     public void tambahItem(String nama, float harga){
